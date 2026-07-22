03d24b813db76be29b8d1e71ea700778f16652a5
uma mahesh <mallemahesh77@gmail.com>
2026-07-22 10:18:53 +0530

    intial some changes

--- ai_agent.py
+++ ai_agent.py
@@ -68,4 +68,4 @@ while True:
     print("-" * 50)
     
 ## code is tell About the weather information for different cities using a Groq LLM and LangChain tools. The agent responds to user queries about the weather in specified cities.
-## and then it provides the user with the requested weather information.
+## and thens it provides the user with the requested weather information.
